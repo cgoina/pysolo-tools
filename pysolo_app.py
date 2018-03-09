75f89b64dbb22965b9b82d6d629afad2c5f55110
Cristian Goina <goinac@janelia.hhmi.org>
2018-03-08 23:14:25 -0500

    added region controls

--- pysolo_app.py
+++ pysolo_app.py
@@ -4,38 +4,44 @@ import numpy as np
 import sys
 
 import cv2
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, QPoint, QRect
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QPoint, QRect, Qt, QObject
 from PyQt5.QtGui import QImage, QPainter, QPixmap
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QMainWindow, QHBoxLayout,
-                             QLabel, QLineEdit, QGridLayout, QFileDialog, QScrollArea, QVBoxLayout, QSpinBox, QComboBox)
+                             QLabel, QLineEdit, QGridLayout, QFileDialog, QScrollArea, QVBoxLayout, QSpinBox, QComboBox,
+                             QGroupBox, QCheckBox)
 
 from pysolo_video import MovieFile
 
 
 class PySoloMainAppWindow(QMainWindow):
-    video_loaded_signal = pyqtSignal(MovieFile)
 
     def __init__(self, parent=None):
         super(PySoloMainAppWindow, self).__init__(parent)
-        self._monitor_widget = MonitorWidget(self)
-        self._form_widget = FormWidget(self, self.video_loaded_signal)
+        communication_channels = WidgetCommunicationChannels()
+        self._monitor_widget = MonitorWidget(self, communication_channels)
+        self._form_widget = FormWidget(self, communication_channels)
         mainWidget = QWidget()
         layout = QHBoxLayout(mainWidget)
         layout.addWidget(self._monitor_widget)
         layout.addWidget(self._form_widget)
         self.setCentralWidget(mainWidget)
-        self.video_loaded_signal.connect(self._monitor_widget.set_movie)
+
+
+class WidgetCommunicationChannels(QObject):
+    video_loaded_signal = pyqtSignal(MovieFile)
+    region_selected_signal = pyqtSignal(int)
 
 
 class MonitorWidget(QWidget):
 
-    def __init__(self, parent, image_width=640, image_height=480):
+    def __init__(self, parent, communication_channels, image_width=640, image_height=480):
         super(MonitorWidget, self).__init__(parent)
         self._image_width = image_width
         self._image_height = image_height
         self._ratio = image_width / image_height
         self._image = QImage()
         self._initUI()
+        communication_channels.video_loaded_signal.connect(self.set_movie)
 
     def _initUI(self):
         self.video_frame = QLabel()
@@ -70,14 +76,14 @@ class MonitorWidget(QWidget):
 
 class CommonOptionsFormWidget(QWidget):
 
-    def __init__(self, parent, video_loaded_signal, max_monitored_areas=64):
+    def __init__(self, parent, communication_channels, max_monitored_areas=64):
         super(CommonOptionsFormWidget, self).__init__(parent)
-        self.video_loaded_signal = video_loaded_signal
+        self._communication_channels = communication_channels
         self._max_monitored_areas = max_monitored_areas
         self._initUI()
 
     def _initUI(self):
-        layout = QGridLayout()
+        group_layout = QGridLayout()
 
         current_layout_row = 0
         # source file name widgets
@@ -86,10 +92,10 @@ class CommonOptionsFormWidget(QWidget):
         source_filename_lbl = QLabel("Select source file")
         source_filename_btn = QPushButton("Open...")
         # add the source filename control to the layout
-        layout.addWidget(source_filename_lbl, current_layout_row, 0)
+        group_layout.addWidget(source_filename_lbl, current_layout_row, 0)
         current_layout_row += 1
-        layout.addWidget(self._source_filename_txt, current_layout_row, 0)
-        layout.addWidget(source_filename_btn, current_layout_row, 1)
+        group_layout.addWidget(self._source_filename_txt, current_layout_row, 0)
+        group_layout.addWidget(source_filename_btn, current_layout_row, 1)
         current_layout_row += 1
         # source file name event handlers
         source_filename_btn.clicked.connect(self._select_source_file)
@@ -100,13 +106,26 @@ class CommonOptionsFormWidget(QWidget):
         results_dir_lbl = QLabel("Select results directory")
         results_dir_btn = QPushButton("Select...")
         # add the source filename control to the layout
-        layout.addWidget(results_dir_lbl, current_layout_row, 0)
+        group_layout.addWidget(results_dir_lbl, current_layout_row, 0)
         current_layout_row += 1
-        layout.addWidget(self._results_dir_txt, current_layout_row, 0)
-        layout.addWidget(results_dir_btn, current_layout_row, 1)
+        group_layout.addWidget(self._results_dir_txt, current_layout_row, 0)
+        group_layout.addWidget(results_dir_btn, current_layout_row, 1)
         current_layout_row += 1
         # results directory event handlers
         results_dir_btn.clicked.connect(self._select_results_dir)
+
+        # size
+        size_lbl = QLabel("Size")
+        group_layout.addWidget(size_lbl, current_layout_row, 0)
+        current_layout_row += 1
+        size_widget = QWidget()
+        self._min_width = QSpinBox()
+        self._max_width = QSpinBox()
+        size_layout = QHBoxLayout(size_widget)
+        size_layout.addWidget(self._min_width, Qt.AlignLeft)
+        size_layout.addWidget(self._max_width, Qt.AlignLeft)
+        group_layout.addWidget(size_widget, current_layout_row, 0)
+        current_layout_row += 1
 
         # number of monitored regions widgets
         self._n_monitored_areas_spinner = QSpinBox()
@@ -114,9 +133,9 @@ class CommonOptionsFormWidget(QWidget):
         self._n_monitored_areas_spinner.setMaximum(self._max_monitored_areas)
         n_monitored_areas_lbl = QLabel("Number of monitored regions")
         # add the number of monitored regions control to the layout
-        layout.addWidget(n_monitored_areas_lbl, current_layout_row, 0)
+        group_layout.addWidget(n_monitored_areas_lbl, current_layout_row, 0)
         current_layout_row += 1
-        layout.addWidget(self._n_monitored_areas_spinner, current_layout_row, 0)
+        group_layout.addWidget(self._n_monitored_areas_spinner, current_layout_row, 0)
         current_layout_row += 1
         # number of monitored regions event handlers
         self._n_monitored_areas_spinner.valueChanged.connect(self._update_number_of_regions)
@@ -126,13 +145,18 @@ class CommonOptionsFormWidget(QWidget):
         self.selected_region.setDisabled(True)
         selected_region_lbl = QLabel("Select region")
         # add selected region control to the layout
-        layout.addWidget(selected_region_lbl, current_layout_row, 0)
+        group_layout.addWidget(selected_region_lbl, current_layout_row, 0)
         current_layout_row += 1
-        layout.addWidget(self.selected_region, current_layout_row, 0)
+        group_layout.addWidget(self.selected_region, current_layout_row, 0)
         current_layout_row += 1
         # current region event handlers
         self.selected_region.currentIndexChanged.connect(self._update_selected_region)
+
         # set the layout
+        groupBox = QGroupBox()
+        groupBox.setLayout(group_layout)
+        layout = QVBoxLayout()
+        layout.addWidget(groupBox)
         self.setLayout(layout)
 
     def _select_source_file(self):
@@ -143,7 +167,7 @@ class CommonOptionsFormWidget(QWidget):
                                                   options=options)
         if fileName:
             self._source_filename_txt.setText(fileName)
-            self.video_loaded_signal.emit(MovieFile(fileName))
+            self._communication_channels.video_loaded_signal.emit(MovieFile(fileName))
 
     def _select_results_dir(self):
         options = QFileDialog.Options(QFileDialog.DontUseNativeDialog | QFileDialog.ShowDirsOnly)
@@ -168,21 +192,124 @@ class CommonOptionsFormWidget(QWidget):
             for r in range(n_regions, new_regions_counter):
                 self.selected_region.addItem('Region %d' % (r + 1), r)
             self.selected_region.setDisabled(False)
+        self._communication_channels.region_selected_signal.emit(new_regions_counter)
 
     def _update_selected_region(self):
         pass
 
 
+class MonitoredAreaFormWidget(QWidget):
+
+    def __init__(self, parent, communication_channels):
+        super(MonitoredAreaFormWidget, self).__init__(parent)
+        self._communication_channels = communication_channels
+        self._initUI()
+        communication_channels.region_selected_signal.connect(self._update_selected_region)
+
+    def _initUI(self):
+        group_layout = QGridLayout()
+
+        current_layout_row = 0
+        # source file name widgets
+        self._mask_filename_txt = QLineEdit()
+        self._mask_filename_txt.setDisabled(True)
+        mask_filename_lbl = QLabel("Select mask file")
+        mask_filename_btn = QPushButton("Open...")
+        # add the mask filename control to the layout
+        group_layout.addWidget(mask_filename_lbl, current_layout_row, 0)
+        current_layout_row += 1
+        group_layout.addWidget(self._mask_filename_txt, current_layout_row, 0)
+        group_layout.addWidget(mask_filename_btn, current_layout_row, 1)
+        current_layout_row += 1
+        # source file name event handlers
+        mask_filename_btn.clicked.connect(self._select_mask_file)
+
+        # track type
+        self._track_type = QComboBox()
+        self._track_type.addItem("Distance", 0)
+        self._track_type.addItem("Crossover", 1)
+        self._track_type.addItem("Position", 2)
+
+        track_type_lbl = QLabel("Select Track Type")
+        # add track type control to the layout
+        group_layout.addWidget(track_type_lbl, current_layout_row, 0)
+        current_layout_row += 1
+        group_layout.addWidget(self._track_type, current_layout_row, 0)
+        current_layout_row += 1
+
+        # track flag
+        self._track_flag = QCheckBox()
+        track_flag_lbl = QLabel("Monitor area")
+        track_flag_widget = QWidget()
+        track_flag_layout = QHBoxLayout(track_flag_widget)
+        track_flag_layout.addWidget(self._track_flag)
+        track_flag_layout.addWidget(track_flag_lbl, Qt.AlignLeft)
+        group_layout.addWidget(track_flag_widget, current_layout_row, 0)
+
+        current_layout_row += 1
+
+        # sleep deprivation flag
+        self._sleep_deprivation_flag = QCheckBox()
+        sleep_deprivation_lbl = QLabel("Sleep deprivation")
+        sleep_deprivationwidget = QWidget()
+        sleep_deprivation_layout = QHBoxLayout(sleep_deprivationwidget)
+        sleep_deprivation_layout.addWidget(self._sleep_deprivation_flag)
+        sleep_deprivation_layout.addWidget(sleep_deprivation_lbl, Qt.AlignLeft)
+        group_layout.addWidget(sleep_deprivationwidget, current_layout_row, 0)
+        current_layout_row += 1
+
+        # Aggregation interval
+        aggregation_interval_lbl = QLabel("Aggregation interval")
+        group_layout.addWidget(aggregation_interval_lbl, current_layout_row, 0)
+        current_layout_row += 1
+        aggregation_interval_widget = QWidget()
+        self._aggregation_interval = QSpinBox()
+        self._aggregation_interval.setMinimum(1)
+        self._aggregation_interval_units = QComboBox()
+        self._aggregation_interval_units.addItems(['frames', 'seconds', 'minutes'])
+        aggregation_interval_layout = QHBoxLayout(aggregation_interval_widget)
+        aggregation_interval_layout.addWidget(self._aggregation_interval, Qt.AlignLeft)
+        aggregation_interval_layout.addWidget(self._aggregation_interval_units, Qt.AlignLeft)
+        group_layout.addWidget(aggregation_interval_widget, current_layout_row, 0)
+        current_layout_row += 1
+
+        # set the layout
+        groupBox = QGroupBox()
+        groupBox.setLayout(group_layout)
+        layout = QVBoxLayout()
+        layout.addWidget(groupBox)
+        self.setLayout(layout)
+
+    def _select_mask_file(self):
+        options = QFileDialog.Options(QFileDialog.DontUseNativeDialog)
+        fileName, _ = QFileDialog.getOpenFileName(self, "Select mask file",
+                                                  self._mask_filename_txt.text(),
+                                                  filter='All files (*)',
+                                                  options=options)
+        if fileName:
+            self._mask_filename_txt.setText(fileName)
+
+    @pyqtSlot(int)
+    def _update_selected_region(self, region):
+        if region == 0:
+            self.setDisabled(True)
+        else:
+            self.setDisabled(False)
+
 class FormWidget(QWidget):
 
-    def __init__(self, parent, video_loaded_signal):
+    def __init__(self, parent, communication_channels):
         super(FormWidget, self).__init__(parent)
-        self._initUI(video_loaded_signal)
+        self._communication_channels = communication_channels
+        self._initUI()
 
-    def _initUI(self, video_loaded_signal):
+    def _initUI(self):
         layout = QGridLayout()
-        commonOptionsWidget = CommonOptionsFormWidget(self, video_loaded_signal)
-        layout.addWidget(commonOptionsWidget, 0, 0)
+        commonOptionsFormWidget = CommonOptionsFormWidget(self, self._communication_channels)
+        monitoredAreaFormWidget = MonitoredAreaFormWidget(self, self._communication_channels)
+        monitoredAreaFormWidget.setDisabled(True)
+        layout.addWidget(commonOptionsFormWidget, 0, 0)
+        layout.addWidget(monitoredAreaFormWidget, 1, 0)
         self.setLayout(layout)
 
 
