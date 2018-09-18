2011fc48b8a112ab7db014b660974afa05e3fa4a
Cristian Goina <goinac@janelia.hhmi.org>
2018-09-18 16:43:46 -0400

    set path to a default miniconda install

--- pysolo_app.bat
+++ pysolo_app.bat
@@ -1,4 +1,11 @@
 @echo off
 
-call conda activate pysolo-tools
+rem for miniconda3 installed in the default user's directory use the path below
+rem otherwise set it to your miniconda install directory
+set MINICONDA_DIR=%HOMEPATH%\AppData\Local\Continuum\miniconda3
+
+set PATH=%MINICONDA_DIR%;%MINICONDA_DIR%\Scripts;%MINICONDA_DIR%\lib;%PATH%
+
+call activate pysolo-tools
+
 python pysolo_app.py %*
